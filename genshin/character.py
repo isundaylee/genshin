3d48e9377004d6aef41adf952245ec8c11306346
Jiahao Li <reg@ljh.me>
2022-07-09 20:40:12 -0700

    Character type setup

---
+++ genshin/character.py
@@ -0,0 +1,46 @@
+from genshin import artifact
+from typing import List, Tuple
+import attr
+
+
+_LEVEL_CAPS: List[int] = [20, 40, 50, 60, 70, 80, 90]
+
+
+@attr.define
+class Character:
+    ascension: int
+    level: int
+    constellations: int
+
+    # These do NOT include the +3s from constellations
+    talent_level_a: int
+    talent_level_e: int
+    talent_level_q: int
+
+    artifacts: Tuple[
+        artifact.Artifact,
+        artifact.Artifact,
+        artifact.Artifact,
+        artifact.Artifact,
+        artifact.Artifact,
+    ]
+
+    def __attrs_post_init__(self):
+        assert 0 <= self.ascension <= 6
+
+        level_lower_bound = (
+            0 if self.ascension == 9 else _LEVEL_CAPS[self.ascension - 1]
+        )
+        level_upper_bound = _LEVEL_CAPS[self.ascension]
+        assert level_lower_bound <= self.level <= level_upper_bound
+
+        assert 1 <= self.talent_level_a <= 10
+        assert 1 <= self.talent_level_e <= 10
+        assert 1 <= self.talent_level_q <= 10
+
+        assert len(self.artifacts) == 5
+        assert self.artifacts[0].artifact_slot == artifact.ArtifactSlot.Flower
+        assert self.artifacts[1].artifact_slot == artifact.ArtifactSlot.Feather
+        assert self.artifacts[2].artifact_slot == artifact.ArtifactSlot.Sand
+        assert self.artifacts[3].artifact_slot == artifact.ArtifactSlot.Cup
+        assert self.artifacts[4].artifact_slot == artifact.ArtifactSlot.Circlet
---
+++ tools/generate_gcsim_config.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from genshin import artifact, character
+import click
+
+
+@click.command()
+def main():
+    yanfei = character.Character(
+        ascension=6,
+        level=90,
+        constellations=6,
+        talent_level_a=10,
+        talent_level_e=10,
+        talent_level_q=10,
+        artifacts=tuple(
+            artifact.parse_artifact(a)
+            for a in (
+                "CW@1@20 HP=4780 EM=23 CD%=18.7 ATK=37 CR%=11.7",
+                "CW@2@20 ATK=311 CR%=9.7 EM=56 CD%=7.8 ER%=4.5",
+                "CW@3@20 EM=187 CR%=12.4 ATK=19 CD%=13.2 ER%=6.5",
+                "LW@4@20 EDP%=46.6 CD%=12.4 HP=448 CR%=10.1 EM=23",
+                "CW@5@20 CR%=31.1 CD%=33.4 HP=209 ATK=18 DEF%=5.8",
+            )
+        ),
+    )
+
+
+if __name__ == "__main__":
+    main()
