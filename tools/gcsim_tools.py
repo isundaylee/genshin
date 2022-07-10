bba8fb9fc4d8e9768f136a1998534a746b41a484
Jiahao Li <reg@ljh.me>
2022-07-10 12:20:14 -0700

    Adds gcsim damage display tool

---
+++ tools/gcsim_tools.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import json
+import click
+import pathlib
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command("show-damages")
+@click.argument("result-file", type=pathlib.Path)
+def do_show_damages(result_file):
+    with open(result_file) as f:
+        raw_data = json.load(f)
+        character_names = raw_data["char_names"]
+        data = json.loads(raw_data["debug"])
+
+    for e in data:
+        if e["event"] != "damage":
+            continue
+
+        print(
+            "{:15s} | {:30s} | {:5s} {:10s} | {:10.0f}".format(
+                character_names[e["char_index"]],
+                e["msg"],
+                "crit" if e["logs"]["crit"] else "",
+                e["logs"]["amp"],
+                e["logs"]["damage"],
+            )
+        )
+
+
+if __name__ == "__main__":
+    main()
