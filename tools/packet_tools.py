40a21c3929156e6933fd9e6c99ee47a2ec91251f
Jiahao Li <reg@ljh.me>
2024-09-05 00:51:14 -0400

    Make packet_tools.py multitool

--- tools/packet_tools.py
+++ tools/packet_tools.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
-import collections
 import pathlib
-import re
-from genshin import character, weapon
 import logging
-from typing import DefaultDict, Dict, List
 
 import click
 
-from genshin.packet import session, opcodes
-from genshin.formats import official
-
-from genshin.packet.proto.PlayerStoreNotify_pb2 import PlayerStoreNotify
-from genshin.packet.proto.StoreType_pb2 import StoreType
+from genshin.packet import session
 
 
-@click.command()
+@click.group()
+def main() -> None:
+    pass
+
+
+@main.command()
 @click.argument("path")
 @click.argument("my_ip")
 @click.argument("output", type=pathlib.Path)
-def main(path: str, my_ip: str, output: pathlib.Path) -> None:
+def dump_decrypted(path: str, my_ip: str, output: pathlib.Path) -> None:
     logging.basicConfig(
         format="%(asctime)s %(levelname)-10s %(name)-60s %(message)s",
         level=logging.INFO,
